ef700dc8df429139d16ac513e9126993cc6148bc
Gonzalo <gonzalogontad@gmail.com>
2021-05-10 14:00:27 -0300

    Cambio en la estructura de codigo

--- HTML_parse.py
+++ HTML_parse.py
@@ -3,46 +3,46 @@ import requests
 import re
 import csv 
 
-URL = 'http://josecarluccio.blogspot.com/2010/02/argentina-1ra-b-afa-1971.html'
-response = requests.get(URL)
-#print (response.text)
-pattern = r'(\S*\/\S*\/\S*) en (\S*): (\D*) (\d*)\s?(\((.*)\))?, (\D*) (\d*)\s?(\((.*)\))?\n?(Nota:\s?.*.)?'
-page = BeautifulSoup(response.text, 'html.parser')
+def HTML_scores_parse (URL):
 
+    response = requests.get(URL)
+    page = BeautifulSoup(response.text, 'html.parser')
 
+    mydivs = page.find_all("div", {"class": "post-body entry-content"})
+    mydivs=str(mydivs)
+    x = mydivs.find("1ra. Fecha")
+    mydivs=mydivs[x:]
+    HTML_LABELS=   [('<br/>','\n'),
+                    ('<em>',''),
+                    ('<span style="color:#33cc00;">',''),
+                    ('<span style="color:#cc66cc;">',''),
+                    ('<strong>',''),
+                    ('</strong>',''),
+                    ('</em>',''),
+                    ('</span>',''),
+                    ('<div style="clear: both;">',''),
+                    ('<div>',''),
+                    ('</div>','')]
 
-mydivs = page.find_all("div", {"class": "post-body entry-content"})
-#print (type(mydivs))
-mydivs=str(mydivs)
-x = mydivs.find("1ra. Fecha")
-mydivs=mydivs[x:]
+    for label in HTML_LABELS:
+        mydivs=mydivs.replace(label[0], label[1])                
 
-mydivs=mydivs.replace('<br/>','\n')
-mydivs=mydivs.replace('<em>','')
-mydivs=mydivs.replace('<span style="color:#33cc00;">','')
-mydivs=mydivs.replace('<span style="color:#cc66cc;">','')
-mydivs=mydivs.replace('<strong>','')
-mydivs=mydivs.replace('</strong>','')
-mydivs=mydivs.replace('</em>','')
-mydivs=mydivs.replace('</span>','')
-mydivs=mydivs.replace('<div style="clear: both;">','')
-mydivs=mydivs.replace('<div>','')
-mydivs=mydivs.replace('</div>','')
-#print (mydivs)
-partidos = re.findall(pattern, mydivs)
-for partido in partidos:
-    print (partido)
+    with open('raw.txt', 'w') as file:
+        file.write (mydivs)
 
-  
-lol = [(1,2,3),(4,5,6),(7,8,9)]
-item_length = len(lol[0])
+    pattern = r'(\S*\/\S*\/\S*) en (\S*): (\D*) (\d*)\s?(\((.*)\))?, (\D*) (\d*)\s?(\((.*)\))?\n?(Nota:\s?.*.)?'
+    partidos = re.findall(pattern, mydivs)
+    for partido in partidos:
+        print (partido)
 
-with open('test.csv', 'w') as file:
-    file_writer = csv.writer(file)
-    for row in partidos:
-        file_writer.writerow(row)
+    with open('test.csv', 'w') as file:
+        file_writer = csv.writer(file)
+        for row in partidos:
+            file_writer.writerow(row)
 
-
+if __name__ == "__main__":
+    URL = 'http://josecarluccio.blogspot.com/2010/02/argentina-1ra-b-afa-1971.html'
+    HTML_scores_parse(URL)
 '''
 20/03/1971 en Mataderos: Nueva Chicago 10 (Jorge Jhones y Jorge Pietrone), All Boys 2 (Luis Medina (p) y Antonio Borruto)
 Nota: Se jugó en cancha de Argentinos Juniors.
